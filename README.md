e3e171cb918f1a07240f7ba5a14899168a3d995a
CaoAnda <1401650042@qq.com>
2022-11-04 07:41:08 +0000

    homework finished

--- README.md
+++ README.md
@@ -5,7 +5,26 @@
 
 实验结果：
 
-|         | LeNet  | LeNet+Half Conv | LeNet+0.05-Dropout | LeNet+0.1-Dropout | LeNet+20x20-Input |
+|         | LeNet-5  | LeNet-5+Half Conv | LeNet-5+0.05-Dropout | LeNet-5+0.1-Dropout | LeNet-5+20x20-Input |
 | ------- | ------ | --------------- | ------------------ | ----------------- | ----------------- |
-| **Acc** | 0.9861 | 0.9776          | 0.9846             | 0.9862            | 0.9835            |
+| **Acc** | 0.9861 | 0.9776          | 0.9846             | **0.9862**        | 0.9835            |
 
+LeNet-5:
+
+`python main.py`
+
+LeNet-5+Half Conv:
+
+`python main.py --half_conv True`
+
+LeNet-5+0.05-Dropout:
+
+`python main.py --dropout 0.05`
+
+LeNet-5+0.1-Dropout:
+
+`python main.py --dropout 0.1`
+
+LeNet-5+20x20-Input:
+
+`python main.py --input_size 20`
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--device', type=str, default='cuda:0')  
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--epochs', type=int, default=40)
-    parser.add_argument('--lr', type=float, default=1e-2)
+    parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--seed', type=int, default=3407)
     parser.add_argument('--half_conv', type=bool, default=False)
     parser.add_argument('--dropout', type=float, default=0)
@@ -18,11 +18,3 @@ def get_args(parser=argparse.ArgumentParser()):
   
 if __name__ == '__main__':  
     opt = get_args()
-    # import os
-    # if not os.path.exists('./analys.csv'):
-    #     with open('./analys.csv', 'w+') as file:
-    #         file.write(','.join(['half_conv', 'dropout', 'input_size', 'lr', 'acc']))
-    #         file.write('\n')
-    # with open('./analys.csv', 'a+') as file:
-    #     file.write(','.join(map(str, [False, 0.2, (32, 32)[0], 1e-3, 0.9445])))
-    #     file.write('\n')
